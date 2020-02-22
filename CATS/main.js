e54daccd6987ce4190f20e739190f099854542e5
Edaz <46797041+edazpotato@users.noreply.github.com>
2020-02-22 20:31:52 +1300

    Update main.js

--- CATS/main.js
+++ CATS/main.js
@@ -1,33 +1,12 @@
 /* eslint no-console: ["error", { allow: ["warn", "error"] }] */
 window.addEventListener("load", () => {
-	function updateElements(data) {
-		var imgSrc = data.file;
+	function updateElements() {
+		var imgSrc = "https://cataas.com/cat/";
 		var titleText = "Meooow";
 		document.getElementById("Img").src = imgSrc;
 		document.title = titleText;
 		document.getElementById("titleSpace").innerHTML = titleText;
-		document.getElementById("desc").innerHTML = "Thanks to <a href=\"https://random.cat\">random.cat</a> for the cat images!";
+		document.getElementById("desc").innerHTML = "Thanks to <a href=\"https://cataas.com\">CAT AS A SERVICE</a> for the cat images!";
 	}
-	//something
-	var getJSON = function(url, callback) {
-		var xhr = new XMLHttpRequest();
-		xhr.open("GET", url, true);
-		xhr.responseType = "json";
-		xhr.onload = function() {
-			var status = xhr.status;
-			if (status === 200) {
-				callback(null, xhr.response);
-			} else {
-				callback(status, xhr.response);
-			}
-		};
-		xhr.send();
-	};
-	getJSON("https://aws.random.cat/meow", (err, data) => {
-		if (err !== null) {
-			console.error("Something went wrong: " + err);
-		} else {
-			updateElements(data);
-		}
-	});
+	updateElements();
 });
