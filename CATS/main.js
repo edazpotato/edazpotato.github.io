c66a045fc1b5546e2a15e67cc16db6bd18daa55e
Edaz <46797041+edazpotato@users.noreply.github.com>
2020-02-26 10:57:20 +1300

    Update main.js

--- CATS/main.js
+++ CATS/main.js
@@ -1,12 +1,26 @@
 /* eslint no-console: ["error", { allow: ["warn", "error"] }] */
 window.addEventListener("load", () => {
-	function updateElements() {
-		var imgSrc = "https://cataas.com/cat/";
+	function updateElements(data) {
+		var imgSrc = data[0].url;
 		var titleText = "Meooow";
 		document.getElementById("Img").src = imgSrc;
 		document.title = titleText;
 		document.getElementById("titleSpace").innerHTML = titleText;
-		document.getElementById("desc").innerHTML = "Thanks to <a href=\"https://cataas.com\">CAT AS A SERVICE</a> for the cat images!";
-	}
-	updateElements();
+		document.getElementById("desc").innerHTML = "Thanks to <a href=\"https://thecatapi.com\">TheCatAPI.com</a> for the cat images!";
+	};
+	const url = new URL("https://api.thecatapi.com/v1/images/search");
+	let xhr = new XMLHttpRequest();
+	xhr.open("GET", url);
+	xhr.responseType = "json";
+	xhr.setRequestHeader("x-api-key", '123');
+	xhr.send();
+	
+	xhr.onload = function() {
+	  console.log(`Loaded: ${xhr.status} ${xhr.response}`);
+	  updateElements(xhr.response)
+	};
+
+	xhr.onerror = function(e) { // only triggers if the request couldn't be made at all
+	  console.error(`Network Error - ${e}`);
+	};
 });
