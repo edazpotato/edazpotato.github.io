1e84927e0a5375feb9eb04443f2062dceba2446c
Edaz <46797041+edazpotato@users.noreply.github.com>
2020-03-06 10:18:24 +1300

    Update main.js

--- SPACEX/main.js
+++ SPACEX/main.js
@@ -1,32 +1,29 @@
 /* eslint no-console: ["error", { allow: ["warn", "error"] }] */
 function loadAndUpdate() {
+    function formatUnixTime(unix_timestamp){
+        // Create a new JavaScript Date object based on the timestamp
+        // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+        var date = new Date(unix_timestamp * 1000);
+        // Hours part from the timestamp
+        var hours = date.getHours();
+        // Minutes part from the timestamp
+        var minutes = "0" + date.getMinutes();
+        // Seconds part from the timestamp
+        var seconds = "0" + date.getSeconds();
+
+        // Will display time in 10:30:23 format
+        var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+    }
     function updateElements(data) {
-        var imgSrc = data.url;
-        var titleText = data.title;
-        var creditText = "That meme was from r/" + data.subreddit + "!";
-        var postLink = data.postLink;
-        document.getElementById("memeImg").src = imgSrc;
-        document.title = titleText;
-        document.getElementById("titleSpace").innerHTML = titleText;
-        document.getElementById("credit").innerHTML = creditText;
-        document.getElementById("postLink").setAttribute("href", postLink);
+        document.getElementById("launchSiteName").innerText = data.launch_site.site_name_long;
+        document.getElementById("flightNumber").innerText = data.flightNumber;
+        var formattedTime = formatUnixTime(data.launch_date_unix);
+        
+        document.getElementById("flightDate").innerText = formattedTime;
+        
     }
     //somehing
-    var getJSON = function (url, callback) {
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", url, true);
-        xhr.responseType = "json";
-        xhr.onload = function () {
-            var status = xhr.status;
-            if (status === 200) {
-                callback(null, xhr.response);
-            } else {
-                callback(status, xhr.response);
-            }
-        };
-        xhr.send();
-    };
-    getJSON("https://meme-api.herokuapp.com/gimme", (err, data) => {
+    getJSON("https://api.spacexdata.com/v3/launches/next", (err, data) => {
         if (err !== null) {
             console.error("Something went wrong: " + err);
         } else {
