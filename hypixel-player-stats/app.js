646f036eda9b86e616bc15ad7e605ec37146c464
Edaz <46797041+edazpotato@users.noreply.github.com>
2020-03-14 17:12:35 +1300

    Create app.js

---
+++ hypixel-player-stats/app.js
@@ -0,0 +1,29 @@
+async function getPlayerUUID(playerName) {
+  let url = "https://api.mojang.com/users/profiles/minecraft/"+playerName;
+  let response = await fetch(url);
+
+  if (response.ok) {
+    let json = await response.json();
+    return json.id;
+  } else {
+    console.log("HTTP-Error: " + response.status);
+  }
+}
+async function getHyixelPlayerStats(uuid, key) {
+  let url = "https://api.hypixel.net/player?";
+  url += "uuid="+uuid;
+  url += "&";
+  url += "key="+key;
+  let response = await fetch(url);
+
+  if (response.ok) {
+    let json = await response.json();
+    return json.id;
+  } else {
+    console.log("HTTP-Error: " + response.status);
+  }
+}
+
+let name  = "EdazPotato";
+let uuid = getPlayerUUID(name);
+console.log(uuid);
