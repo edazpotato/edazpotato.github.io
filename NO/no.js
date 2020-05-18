d5dfb511cfb5bbdaee05b219fef793f28d474dfa
Edaz <edazpotato@gmail.com>
2020-05-18 13:11:44 +1200

    Create no.js

---
+++ NO/no.js
@@ -0,0 +1,40 @@
+/**
+ * document.exitFullScreen() polyfill
+ * @author Chris Ferdinandi
+ * @license MIT
+ */
+if (!document.exitFullscreen) {
+	document.exitFullscreen = document.mozExitFullscreen || document.webkitExitFullscreen || document.msExitFullscreen;
+}
+
+/**
+ * document.fullscreenElement polyfill
+ * Adapted from https://shaka-player-demo.appspot.com/docs/api/lib_polyfill_fullscreen.js.html
+ * @author Chris Ferdinandi
+ * @license MIT
+ */
+if (!document.fullscreenElement) {
+
+	Object.defineProperty(document, 'fullscreenElement', {
+		get: function() {
+			return document.mozFullScreenElement || document.msFullscreenElement || document.webkitFullscreenElement;
+		}
+	});
+
+	Object.defineProperty(document, 'fullscreenEnabled', {
+		get: function() {
+			return document.mozFullScreenEnabled || document.msFullscreenEnabled || document.webkitFullscreenEnabled;
+		}
+	});
+}
+
+window.onload = function() {
+	// If there's an element in fullscreen, exit
+	// Otherwise, enter it
+	if (document.fullscreenElement) {
+		document.exitFullscreen();
+	} else {
+		document.documentElement.requestFullscreen();
+	}
+
+}
