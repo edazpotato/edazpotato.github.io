b1402f3c464190553855ca7e3466cb40e821873c
Edaz <edazpotato@gmail.com>
2020-06-24 20:56:22 +1200

    Update blog.js

--- blog/js/blog.js
+++ blog/js/blog.js
@@ -1,4 +1,41 @@
+/* helper functions */
+function get(url, json) {
+  var response = await fetch(url);
 
+  if (response.ok) { // if HTTP-status is 200-299
+    var data;
+    if (json) {
+      data = await response.json();
+    } else {
+      data = await response.text();
+    }
+    return data;
+  } else {
+    console.error("HTTP-Error: " + response.status);
+  }
+}
+
+
+/* main blog system */
+if (mode == "postsearch") {
+  /* get list of posts */
+  var posts = await get("posts.json", true);
+  /* get posts */
+  for (post of posts) {
+    get("posts/" + post.path, false);
+  }
+} else if (mode == "postveiw") {
+  /* declare vars */
+  var authorName;
+  var authorImage;
+  var postTitle;
+  var postPath;
+  var postdate;
+  /* get list of posts */
+  var posts = await get("posts.json", true);
+  
+  
+}
 
 
 
