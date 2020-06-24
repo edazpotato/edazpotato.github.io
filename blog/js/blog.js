bb11a96d583a36aa05391952d6d7e71de3993955
Edaz <edazpotato@gmail.com>
2020-06-25 10:14:54 +1200

    Update blog.js

--- blog/js/blog.js
+++ blog/js/blog.js
@@ -1,49 +1,49 @@
-/* helper functions */
-function get(url, json) {
-  var response = await fetch(url);
+function Blog() {
+  
+  /* helper functions */
+  function get (url, json) {
+    var response = await fetch(url);
 
-  if (response.ok) { // if HTTP-status is 200-299
-    var data;
-    if (json) {
-      data = await response.json();
+    if (response.ok) { // if HTTP-status is 200-299
+      var data;
+      if (json) {
+        data = await response.json();
+      } else {
+        data = await response.text();
+      }
+      return data;
     } else {
-      data = await response.text();
+      console.error("HTTP-Error: " + response.status);
     }
-    return data;
-  } else {
-    console.error("HTTP-Error: " + response.status);
   }
-}
 
-/* variables needed:
- * mode: either "home" or "postsearch" or "postveiw"
- * id: id of post, only set if mode is "postveiw"
- */
-/* main blog system */
-if (mode == "postsearch") {
-  /* get list of posts */
-  var posts = await get("posts.json", true);
-  /* get posts */
-  for (post of posts) {
-    get("posts/" + post.path, false);
+  function init() {
+    /* main blog system */
+    if (mode == "postsearch") {
+      /* get list of posts */
+      var posts = await get("posts.json", true);
+      /* get posts */
+      for (post of posts) {
+        get("posts/" + post.path, false);
+      }
+    } else if (mode == "postveiw") {
+      /* get list of posts */
+      var posts = await get("posts.json", true);
+      var post = posts[{"id": id}];
+      /* declare vars */
+      var authorName = post.author;
+      var authorImage = post.author_image_path;
+      var postTitle = post.title;
+      var postPath = post.path;
+      var postdate = post.date; // NOTE: this is not a date object, just a string, because i'm lazy
+
+
+    }
   }
-} else if (mode == "postveiw") {
-  /* get list of posts */
-  var posts = await get("posts.json", true);
-  var post = posts[{"id": id}];
-  /* declare vars */
-  var authorName = post.author;
-  var authorImage = post.author_image_path;
-  var postTitle = post.title;
-  var postPath = post.path;
-  var postdate = post.date; // NOTE: this is not a date object, just a string, because i'm lazy
-  
-  
+
+
+  /* parse markdown to html */
+  text = "";
+  converter = new showdown.Converter(),
+  html = converter.makeHtml(text);
 }
-
-
-
-/* parse markdown to html */
-text = "";
-converter = new showdown.Converter(),
-html = converter.makeHtml(text);
