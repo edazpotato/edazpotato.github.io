479ae45915dfc64e098379e727f8a04f1d064f6e
Edaz Potato <edazpotato@gmail.com>
2020-07-28 16:28:41 +1200

    Woo it works!

--- portfolio/app.js
+++ portfolio/app.js
@@ -1,1 +1,38 @@
 console.log("Yay it works.")
+
+var hoverCards = document.getElementsByClassName("3dhover");
+console.log(hoverCards)
+for (var el of hoverCards) {
+
+	/* 3D hover effect from this article: https://technokami.in/3d-hover-effect-using-javascript-animations-css-html */
+	var height = el.clientHeight
+	var width = el.clientWidth
+
+	el.addEventListener('mousemove', handleMove)
+
+	function handleMove(e) {
+
+	  var xVal = e.layerX;
+	  var yVal = e.layerY;
+	  
+	  var yRotation = 20 * ((xVal - width / 2) / width);
+	  
+	  var xRotation = -20 * ((yVal - height / 2) / height);
+	  
+	  var string = 'perspective(500px) scale(1.1) rotateX(' + xRotation + 'deg) rotateY(' + yRotation + 'deg)';
+	  
+	  el.style.transform = string;
+	}
+
+	el.addEventListener('mouseout', function() {
+	  el.style.transform = 'perspective(500px) scale(1) rotateX(0) rotateY(0)'
+	});
+
+	el.addEventListener('mousedown', function() {
+	  el.style.transform = 'perspective(500px) scale(0.9) rotateX(0) rotateY(0)'
+	});
+
+	el.addEventListener('mouseup', function() {
+	  el.style.transform = 'perspective(500px) scale(1.1) rotateX(0) rotateY(0)'
+	});
+}
