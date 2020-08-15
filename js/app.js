7df7d455300d5afe67c4cf4e2c856fd6827f59db
Edaz Potato <edazpotato@gmail.com>
2020-08-15 19:17:37 +1200

    Finish (mostly) new homepage and switch to it

---
+++ js/app.js
@@ -0,0 +1,40 @@
+var rick = {
+  roll: function(){
+    document.getElementById("rick_modal").checked = true;
+  }
+}
+Mousetrap.bind('up up down down left right left right b a enter', function() {
+    rick.roll();
+});
+
+window.addEventListener('scroll', function() {
+  // Hide arrow button when scrolling down the page 
+  var scrollPosition = window.scrollY; 
+  var arrow = document.getElementById('arrow'); 
+  if (scrollPosition >= 130) {
+    arrow.classList.add('hide'); 
+  } else {
+    arrow.classList.remove('hide'); 
+  }
+  /*var scrollratio = 1 - scrollPosition / 300;
+  arrow.style.opacity = scrollratio;*/
+});
+
+/* picnic.css javascript stuff */
+document.onkeydown = function(e){
+  if (e.keyCode == 27) {
+    var mods = document.querySelectorAll('.modal > [type=checkbox]');
+    [].forEach.call(mods, function(mod){ mod.checked = false; });
+  }
+}
+document.addEventListener("DOMContentLoaded", function() {
+  [].forEach.call(document.querySelectorAll('.dropimage'), function(img){
+    img.onchange = function(e){
+      var inputfile = this, reader = new FileReader();
+      reader.onloadend = function(){
+        inputfile.style['background-image'] = 'url('+reader.result+')';
+      }
+      reader.readAsDataURL(e.target.files[0]);
+    }
+  });
+});
