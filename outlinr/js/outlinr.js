a58189c59e10aa109f784d4798a90e5bbf5b1029
Edaz <edazpotato@gmail.com>
2020-08-18 13:01:47 +1200

    Setup code structure and test links function

--- outlinr/js/outlinr.js
+++ outlinr/js/outlinr.js
@@ -1,1 +1,13 @@
+function updateLinks(e){
+  console.log(e);
+}
 
+function setup(){
+  for (var el of document.getElementById("sidebar-items").children) {
+      el.addEventListener("click", updateLinks);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+  setup();
+});
