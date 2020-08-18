a0c1b56a10a3ffbdc28cfa22cfcfa6ec7d954582
Edaz <edazpotato@gmail.com>
2020-08-18 14:50:55 +1200

    Add tabs logic

--- outlinr/js/outlinr.js
+++ outlinr/js/outlinr.js
@@ -1,13 +1,27 @@
+function updateTabs(){
+  for (var el of document.getElementById("content").children) {
+    var kid = el.children[0];
+    kid.classList.remove("active");
+  }
+  var hash = location.hash;
+  var anchorID = hash.slice(1);
+  var activeTab = document.getElementByID(anchorID);
+  activeTab.classList.add("active");
+}
+
 function updateLinks(e){
-  var source = event.target || event.srcElement;
   for (var el of document.getElementById("sidebar-items").children) {
     var kid = el.children[0];
     kid.classList.remove("active-link");
   }
+  var source = event.target || event.srcElement;
   source.classList.add("active-link");
+  updateTabs();
 }
 
 function setup(){
+  location.hash = "home";
+  updateTabs();
   for (var el of document.getElementById("sidebar-items").children) {
     var kid = el.children[0];
       kid.addEventListener("click", updateLinks);
