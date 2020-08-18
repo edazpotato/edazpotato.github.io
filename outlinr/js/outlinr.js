7fe7e1c4c05fe250d8531ed7688d15f0f07c7d66
Edaz <edazpotato@gmail.com>
2020-08-18 13:14:46 +1200

    Add a ton of logicfor making links toggle

--- outlinr/js/outlinr.js
+++ outlinr/js/outlinr.js
@@ -1,10 +1,18 @@
 function updateLinks(e){
   console.log(e);
+  var source = event.target || event.srcElement;
+  console.log(source);
+  for (var el of document.getElementById("sidebar-items").children) {
+    var kid = el.children[0];
+    kid.classList.remove("active");
+  }
+  source.classList.add("active-link");
 }
 
 function setup(){
   for (var el of document.getElementById("sidebar-items").children) {
-      el.addEventListener("click", updateLinks);
+    var kid = el.children[0];
+      kid.addEventListener("click", updateLinks);
   }
 }
 
