031329f8546c6bf70aa6038a8b23b8d6a36ce42e
Edaz <edazpotato@gmail.com>
2020-08-19 13:21:40 +1200

    Update simp-detector.js

--- simp-o-meter/simp-detector.js
+++ simp-o-meter/simp-detector.js
@@ -5,6 +5,11 @@ function loop(ctx) {
 function setup() {
   var canvas = document.getElementById("simpcanvas");
   var ctx = canvas.getContext("2d");
+  var img = new Image();
+  img.onload = function() {
+    ctx.drawImage(img, 0, 0);
+  }
+  img.src = "https://edazpoato.github.io/assets/simpometer-bg.png";
   loop(ctx)
 }
 document.addEventListener("DOMContentLoaded", setup);
