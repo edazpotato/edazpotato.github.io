d55a52d3e18faf4189f7d8665b98b275fd49e515
Edaz <edazpotato@gmail.com>
2020-08-26 17:17:39 +1200

    Add cache system

--- worker.js
+++ worker.js
@@ -1,1 +1,64 @@
+/* Service workerr woot! */
+// If you're intreseted in service workers, you should read this article: https://developers.google.com/web/fundamentals/primers/service-workers
+
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  '/js/app.js',
+  '/css/styles.css',
+  '/css/picnic.min.css',
+  '/outlinr/',
+  '/outlinr/outlinr.js',
+  '/outlinr/outlinr.css',
+  '/outlinr/picnic.min.css',
+];
+
+
+self.addEventListener('install', function(event) {
+  //stuff that runs on worker install
+  console.log("I'm alive!");
+  
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  //stuff that runs on fetch
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
+});
 
