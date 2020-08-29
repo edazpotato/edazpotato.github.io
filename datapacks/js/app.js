9c345aa8c41a782a1ad6a7687352d49af78cbe01
Edaz Potato <edazpotato@gmail.com>
2020-08-29 21:37:09 +1200

    start work on gallery

--- datapacks/js/app.js
+++ datapacks/js/app.js
@@ -1,6 +1,14 @@
 var app = new Vue({
 	el: "#app",
 	data: {
-		year: new Date().getFullYear()
+		year: new Date().getFullYear(),
+		packs: []
+	},
+	created() {
+		fetch("packs.json")
+			.then(res => res.json())
+			.then(json => {
+				this.packs = json.packs;
+			});
 	}
 });
