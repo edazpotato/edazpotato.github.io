ea68d33015462575179fd157967ab1159312865f
Edaz <edazpotato@gmail.com>
2020-09-04 10:14:34 +1200

    Add localstorage system and setup textarea saving

--- outlinr/js/outlinr.js
+++ outlinr/js/outlinr.js
@@ -1,7 +1,32 @@
+var outlinrData = {
+  "general": {},
+  "notes": {
+    "value": ""
+  },
+  "todos": {}
+};
+function updateStorage(){
+  var storage = window.localStorage;
+  storage.setItem("outlinr", JSON.stringify(outlinrData));
+}
+function loadStorage(){
+  var storage = window.localStorage;
+  outlinrData = storage.getItem("outlinr");
+}
+function setupTextarea(){
+  var textEl = document.getElementById("notes-input");
+  textEl.value = outlinrData.notes.value;
+  textEl.addEventListener("keyup", function(){
+    outlinrData.notes.value = textEl.value;
+    updateStorage()
+  });
+}
 function setup(){
   if (location.hash == ""){
     location.hash = "home";
   }
+  loadStorage();
+  setupTextarea();
 }
 
 document.addEventListener("DOMContentLoaded", function(){
