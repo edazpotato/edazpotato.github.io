477a0cd76fae6aadf6ddc7c526581ec9821ce9dd
Edaz <edazpotato@gmail.com>
2020-09-04 12:48:08 +1200

    Add return messages to functions

--- outlinr/js/outlinr.js
+++ outlinr/js/outlinr.js
@@ -8,14 +8,17 @@ var outlinrData = {
 function updateStorage(){
   var storage = window.localStorage;
   storage.setItem("outlinr", JSON.stringify(outlinrData));
+  return "Storage is now: '" + JSON.stringify(outlinrData) + "'.";
 }
 function loadStorage(){
   var storage = window.localStorage;
   outlinrStorageData = JSON.parse(storage.getItem("outlinr"));
   if (outlinrStorageData == null) {
     updateStorage();
+    return "Nothing in storage. Setting to current text value...";
   } else{
     outlinrData = outlinrStorageData;
+    return "Loaded '" + JSON.stringify(outlinrData) + "' from storage";
   }
 }
 function setupTextarea(){
@@ -25,6 +28,7 @@ function setupTextarea(){
     outlinrData.notes.value = textEl.value;
     updateStorage();
   });
+  return "Text area set up!";
 }
 function setup(){
   if (location.hash == ""){
@@ -32,6 +36,7 @@ function setup(){
   }
   loadStorage();
   setupTextarea();
+  return "Setup complete!";
 }
 
 document.addEventListener("DOMContentLoaded", function(){
