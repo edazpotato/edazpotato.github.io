7aeac665faeb4c348adc649fa55c1823ac5f48ce
Edaz <edazpotato@gmail.com>
2020-09-16 13:25:50 +1200

    Update app.js

--- aitest/app.js
+++ aitest/app.js
@@ -4,7 +4,7 @@ async function recognizeImage() {
   await worker.load();
   await worker.loadLanguage('eng');
   await worker.initialize('eng');
-  const { data: { text } } = await worker.recognize(file, document.getElementById("imgSelect").val());
+  const { data: { text } } = await worker.recognize(file, document.getElementById("imageinput").val());
   dealWithTextData(text);
   await worker.terminate();
 }
