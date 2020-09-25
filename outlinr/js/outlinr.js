c5ec0b64a72b6ecbbb1b120de07494265dd8db01
Edaz <edazpotato@gmail.com>
2020-09-25 12:29:09 +1200

    add data export

--- outlinr/js/outlinr.js
+++ outlinr/js/outlinr.js
@@ -46,6 +46,11 @@ function setupShortcuts(){
     alert("You don't need to save :) - Outlinr saves in your browser automatically!");
   });
 }
+function exportData() {
+  updateStorage();
+  copyTextToClipboard(JSON.stringify(outlinrData));
+  alert("Data copied to clipboard!")
+}
 function setup(){
   loadStorage();
   setupRoutes();
@@ -57,3 +62,39 @@ function setup(){
 document.addEventListener("DOMContentLoaded", function(){
   setup();
 });
+
+/* random helper functios */
+function fallbackCopyTextToClipboard(text) {
+  var textArea = document.createElement("textarea");
+  textArea.value = text;
+  
+  // Avoid scrolling to bottom
+  textArea.style.top = "0";
+  textArea.style.left = "0";
+  textArea.style.position = "fixed";
+
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+
+  try {
+    var successful = document.execCommand('copy');
+    var msg = successful ? 'successful' : 'unsuccessful';
+    console.log('Fallback: Copying text command was ' + msg);
+  } catch (err) {
+    console.error('Fallback: Oops, unable to copy', err);
+  }
+
+  document.body.removeChild(textArea);
+}
+function copyTextToClipboard(text) {
+  if (!navigator.clipboard) {
+    fallbackCopyTextToClipboard(text);
+    return;
+  }
+  navigator.clipboard.writeText(text).then(function() {
+    console.log('Async: Copying to clipboard was successful!');
+  }, function(err) {
+    console.error('Async: Could not copy text: ', err);
+  });
+}
